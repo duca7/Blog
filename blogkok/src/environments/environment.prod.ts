2a5d9e0dd4bfb60ba4aee4a23ec16be5c3dbe344
duc <hohuynhduca7bh123@gmail.com>
2021-11-09 23:10:59 +0700

    enviroment

--- blogkok/src/environments/environment.prod.ts
+++ blogkok/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
+const api = "http://localhost:3000/api/v1";
 export const environment = {
-  production: true
+  production: true,
 };
--- blogkok/src/environments/environment.ts
+++ blogkok/src/environments/environment.ts
@@ -2,6 +2,8 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+const api = "http://localhost:3000/api/v1";
+
 export const environment = {
   production: false
 };
