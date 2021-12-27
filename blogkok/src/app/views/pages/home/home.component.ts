8ad26dc8c420dddf33e05da8436427f5f3ff1d59
duc <hohuynhduca7bh123@gmail.com>
2021-12-27 22:20:13 +0700

    add swiper

--- blogkok/src/app/views/pages/home/home.component.ts
+++ blogkok/src/app/views/pages/home/home.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ArticleResponse } from 'src/app/model/article.model';
 import { PostArticleService } from 'src/app/services/post-article.service';
-
+import { SwiperOptions } from 'swiper';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -25,4 +25,43 @@ export class HomeComponent implements OnInit {
       this.articles = articles;
     });
   }
+
+  Images: Array<object> = [
+    {
+      src: 'https://loremflickr.com/g/600/400/paris',
+      alt: 'Image 1',
+    },
+    {
+      src: 'https://loremflickr.com/600/400/brazil,rio',
+      alt: 'Image 2',
+    },
+    {
+      src: 'https://loremflickr.com/600/400/paris,girl/all',
+      alt: 'Image 3',
+    },
+    {
+      src: 'https://loremflickr.com/600/400/brazil,rio',
+      alt: 'Image 4',
+    },
+    {
+      src: 'https://loremflickr.com/600/400/paris,girl/all',
+      alt: 'Image 5',
+    },
+    {
+      src: 'https://loremflickr.com/600/400/brazil,rio',
+      alt: 'Image 6',
+    },
+  ];
+
+  config: SwiperOptions = {
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    spaceBetween: 30,
+  };
 }
