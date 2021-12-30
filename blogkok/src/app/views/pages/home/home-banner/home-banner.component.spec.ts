240aceeaa1c6e5550c3de906ac130fd623ea089c
duc <hohuynhduca7bh123@gmail.com>
2021-12-30 11:11:46 +0700

    remove dead css

---
+++ blogkok/src/app/views/pages/home/home-banner/home-banner.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { HomeBannerComponent } from './home-banner.component';
+
+describe('HomeBannerComponent', () => {
+  let component: HomeBannerComponent;
+  let fixture: ComponentFixture<HomeBannerComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ HomeBannerComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HomeBannerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- blogkok/src/app/views/pages/home/home.module.ts
+++ blogkok/src/app/views/pages/home/home.module.ts
@@ -8,8 +8,9 @@ import { BaseModule } from '../../base.module';
 import { HttpClientModule } from '@angular/common/http';
 import { MatIconModule } from '@angular/material/icon';
 import { NgxUsefulSwiperModule } from 'ngx-useful-swiper';
+import { HomeBannerComponent } from './home-banner/home-banner.component';
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, HomeBannerComponent],
   imports: [
     CommonModule,
     HomeRoutingModule,
@@ -18,5 +19,6 @@ import { NgxUsefulSwiperModule } from 'ngx-useful-swiper';
     BaseModule,
     NgxUsefulSwiperModule,
   ],
+  exports: [HomeBannerComponent],
 })
 export class HomeModule {}
